69b4204182fb202a0937cf03b985e3a23b4a7690
Pewterschmidt <github@sushigang.online>
2023-01-31 17:59:42 +0100

    Establish "starre Kopplung" by manipulating data in "bestellung.csv" Using numpy library
    Add "bestellung.csv" in .gitignore

--- Fertigung.py
+++ Fertigung.py
@@ -1,16 +1,44 @@
+import numpy as np
 from handshake import Anlage
+import time
 
 anlage = Anlage()
-                
 
-    
+def convert(x):
+    liste = [x[0],x[2],x[4]] # ['1;2;3'] → ['1','2','3']
+    _liste = [int(x) for x in liste] # [1, 2, 3]
+    return _liste
+
+try:
+    with open("./digitalTwin/bestellung.csv", "r") as file:
+
+        bestellung = [i.strip() for i in file.readlines()]
+        bestellung.pop(0) # Spalten Überschrift entfernt
+        print(bestellung)
+except FileNotFoundError: 
+    print("Ich mag kleine 🖐")
 
 
-for i in range(5):
-    order_1 = input("Welche Bestellung?: ")
-    order_2 = input("Bauen: ")
-    order_3 = input("Lager:")
-    
-    anlage.Fertigung(order_1, order_2, order_3)
+else:
+    bestellung += ["0;0;0"] # Zum verschieben am Ende Zwei leere Aufträge
+    bestellung += ["0;0;0"]
+    conv_bestellung = [convert(reihe) for reihe in bestellung]
+    np_bestellung = np.array(conv_bestellung)
+
+    np_bestellung[1:, 1] = np_bestellung[:-1, 1]
+    np_bestellung[1:, 2] = np_bestellung[:-1, 2]
+    np_bestellung[1:, 2] = np_bestellung[:-1, 2]
+    np_bestellung[0, 1] = 0
+    np_bestellung[0, 2] = 0
+    np_bestellung[1, 2] = 0
+
+    print(np_bestellung)
+
+    for i in range(len(np_bestellung)):
 
 
+        anlage.Fertigung(np_bestellung[i][0], np_bestellung[i][1], np_bestellung[i][2])
+        print(i) 
+
+    time.sleep(5)
+    anlage.StopBand()
--- start.py
+++ start.py
@@ -1,7 +1,7 @@
 import subprocess
 
 program1 = "./digitalTwin/digitalTwin2022_v22_12.py"
-program2 = "./digitalTwin/01_uebung_handshake.py"
+program2 = "./digitalTwin/Fertigung.py"
 
 p1 = subprocess.Popen(["python", program1])
 p2 = subprocess.Popen(["python", program2])
